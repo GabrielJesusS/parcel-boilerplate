ca7ac00f48821660dd834f0097bc508894c9c14f
GabrielJesusS <GabrielJesusS@users.noreply.github.com>
2024-08-01 12:29:19 +0000

    deploy: 0fc43d98edf677ad87de539ca4e14e00c7068aab

---
+++ index.b73a468e.js
@@ -0,0 +1,2 @@
+console.log("Script loaded");
+//# sourceMappingURL=index.b73a468e.js.map
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./source/**/*.{html,js,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
